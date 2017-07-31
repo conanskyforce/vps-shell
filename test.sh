19d057b7ef5ca5a1e977eb59ec0cc998758b0ed1
conanskyforce <conanskyforce@gmail.com>
2017-07-31 16:09:57 +0800

    test

---
+++ test.sh
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+wget https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+chmod +x speedtest.py
+./speedtest.py  --server=5083
+./speedtest.py
+./speedtest.py --list
+./speedtest.py --list | grep Shanghai
+./speedtest.py  --server=5083
